e99255a6ae71daf2c604c34b134ea27d8319e4e6
Jarom Rial <jarom.rial@gmail.com>
2022-06-14 14:49:10 -0600

    update admin dashboard href and change 'current' boolean to 'components' array

--- resources/js/Layouts/Admin.vue
+++ resources/js/Layouts/Admin.vue
@@ -9,12 +9,12 @@ import { Link } from '@inertiajs/inertia-vue3';
 
 const showingNavigationDropdown = ref(false);
 const navigation = [
-  { name: 'Dashboard', href: '#', current: true },
-  { name: 'Team', href: '#', current: false },
-  { name: 'Projects', href: '#', current: false },
-  { name: 'Calendar', href: '#', current: false },
-  { name: 'Documents', href: '#', current: false },
-  { name: 'Reports', href: '#', current: false },
+  { name: 'Dashboard', href: route('admin.dashboard'), components: ['Admin/Dashboard'] },
+  { name: 'Team', href: '#', components: [] },
+  { name: 'Projects', href: '#', components: [] },
+  { name: 'Calendar', href: '#', components: [] },
+  { name: 'Documents', href: '#', components: [] },
+  { name: 'Reports', href: '#', components: [] },
 ];
 </script>
 
@@ -109,7 +109,7 @@ const navigation = [
                         <div class="flex-1 flex flex-col min-h-0">
                             <div class="flex-1 flex flex-col py-8 overflow-y-auto">
                                 <nav class="space-y-2 px-4" aria-label="Sidebar">
-                                    <a v-for="item in navigation" :key="item.name" :href="item.href" :class="[item.current ? 'bg-gray-100 text-gray-900' : 'text-gray-600 hover:bg-gray-50 hover:text-gray-900', 'flex items-center px-3 py-2 text-sm font-medium rounded-md']" :aria-current="item.current ? 'page' : undefined">
+                                    <a v-for="item in navigation" :key="item.name" :href="item.href" :class="[item.components.includes($page.component) ? 'bg-gray-100 text-gray-900' : 'text-gray-600 hover:bg-gray-50 hover:text-gray-900', 'flex items-center px-3 py-2 text-sm font-medium rounded-md']" :aria-current="item.current ? 'page' : undefined">
                                         <span class="truncate">
                                             {{ item.name }}
                                         </span>
