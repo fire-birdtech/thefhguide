190bd526c97ef3a779316556d3e7ae64e626c030
Jarom Rial <jarom.rial@gmail.com>
2022-08-03 05:29:18 -0600

    add admin and editor veritical navigation components to admin layout

--- resources/js/Layouts/Admin.vue
+++ resources/js/Layouts/Admin.vue
@@ -6,6 +6,8 @@ import BreezeDropdownLink from '@/Components/DropdownLink.vue';
 import BreezeNavLink from '@/Components/NavLink.vue';
 import BreezeResponsiveNavLink from '@/Components/ResponsiveNavLink.vue';
 import { Link } from '@inertiajs/inertia-vue3';
+import AdminNav from '@/Components/Navigation/AdminVertical.vue';
+import EditorNav from '@/Components/Navigation/EditorVertical.vue';
 
 defineProps({
     type: String,
@@ -111,21 +113,8 @@ const navigation = [
 
             <!-- Page Content -->
             <main class="flex">
-                <aside class="hidden lg:flex lg:flex-shrink-0">
-                    <div class="flex flex-col w-64">
-                        <div class="flex-1 flex flex-col min-h-0">
-                            <div class="flex-1 flex flex-col py-8 overflow-y-auto">
-                                <nav class="space-y-2 px-4" aria-label="Sidebar">
-                                    <Link v-for="item in navigation" :key="item.name" :href="item.href" :class="[item.components.includes($page.component) ? 'bg-gray-200 text-gray-900 font-semibold' : 'text-gray-600 hover:bg-gray-200 hover:text-gray-900', 'flex items-center px-3 py-2 text-sm font-medium rounded-md']" :aria-current="item.current ? 'page' : undefined">
-                                        <span class="truncate">
-                                            {{ item.name }}
-                                        </span>
-                                    </Link>
-                                </nav>
-                            </div>
-                        </div>
-                    </div>
-                </aside>
+                <AdminNav v-if="type === 'admin'" />
+                <EditorNav v-if="type === 'editor'" />
                 <slot />
             </main>
         </div>
