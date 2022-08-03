22abc0a4f7c5b8fef738f3b06eba2caac7e9d4e2
Jarom Rial <jarom.rial@gmail.com>
2022-08-03 05:28:33 -0600

    create vertical navigation component for admins

---
+++ resources/js/Components/Navigation/AdminVertical.vue
@@ -0,0 +1,33 @@
+<script setup>
+const navigation = [
+    { name: 'Dashboard', href: route('admin.dashboard'), components: ['Admin/Dashboard'] },
+    { name: 'Projects', href: route('admin.projects.index'), components: [
+        'Admin/Projects/Index','Admin/Projects/Create','Admin/Projects/Show','Admin/Projects/Edit'
+    ]},
+    { name: 'Partners', href: route('admin.partners.index'), components: [
+        'Admin/Partners/Index','Admin/Partners/Create','Admin/Partners/Show','Admin/Partners/Edit'
+    ]},
+    { name: 'Collections', href: route('admin.collections.index'), components: [
+        'Admin/Collections/Index','Admin/Collections/Create','Admin/Collections/Show','Admin/Collections/Edit'
+    ] },
+    // { name: 'Navigation', href: '#', components: [] },
+];
+</script>
+
+<template>
+    <aside class="hidden lg:flex lg:flex-shrink-0">
+        <div class="flex flex-col w-64">
+            <div class="flex-1 flex flex-col min-h-0">
+                <div class="flex-1 flex flex-col py-8 overflow-y-auto">
+                    <nav class="space-y-2 px-4" aria-label="Sidebar">
+                        <Link v-for="item in navigation" :key="item.name" :href="item.href" :class="[item.components.includes($page.component) ? 'bg-gray-200 text-gray-900 font-semibold' : 'text-gray-600 hover:bg-gray-200 hover:text-gray-900', 'flex items-center px-3 py-2 text-sm font-medium rounded-md']" :aria-current="item.current ? 'page' : undefined">
+                            <span class="truncate">
+                                {{ item.name }}
+                            </span>
+                        </Link>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </aside>
+</template>
