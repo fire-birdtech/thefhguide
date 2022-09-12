9e676a4655f51e52edfc18662cc991a69da6c63a
Jarom Rial <jarom.rial@gmail.com>
2022-09-12 05:58:56 -0600

    add user role to Inertia requests

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -36,7 +36,7 @@ class HandleInertiaRequests extends Middleware
     {
         return array_merge(parent::share($request), [
             'auth' => [
-                'user' => $request->user(),
+                'user' => fn () => auth()->user() ? $request->user()->load('roles') : "",
             ],
             'ziggy' => function () {
                 return (new Ziggy)->toArray();
