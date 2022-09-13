cca12fee14b267ed9f6b17ef1e1ea0f0b9397834
Jarom Rial <jarom.rial@gmail.com>
2022-09-13 06:25:06 -0600

    move card padding to Link

--- resources/js/Pages/Editor/Content/Index.vue
+++ resources/js/Pages/Editor/Content/Index.vue
@@ -14,8 +14,8 @@ defineProps({
         <div class="w-full py-8 px-4 sm:px-6 lg:px-8">
             <ul role="list" class="grid grid-cols-2 gap-6 sm:grid-cols-3 md:grid-cols-5 lg:grid-cols-7">
                 <li v-for="collection in collections" :key="collection.id" class="col-span-1 border border-transparent flex flex-col items-center justify-center rounded-lg bg-white text-center shadow hover:border-indigo-400">
-                    <Link :href="route('admin.collections.show', [collection.slug])">
-                        <div class="flex flex-1 flex-col p-6">
+                    <Link class="p-6" :href="route('admin.collections.show', [collection.slug])">
+                        <div class="flex flex-1 flex-col">
                             <h3 class="text-sm font-medium text-gray-900">{{ collection.name }}</h3>
                         </div>
                     </Link>
