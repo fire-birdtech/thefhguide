665438a3167fc28c7d5f77447d5cd066af138266
Jarom Rial <jarom.rial@gmail.com>
2022-09-13 06:22:26 -0600

    update name of column to add instructions after

--- database/migrations/2022_09_13_120830_add_instructions_to_projects_table.php
+++ database/migrations/2022_09_13_120830_add_instructions_to_projects_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::table('projects', function (Blueprint $table) {
-            $table->longText('instructions')->nullable()->after('projectable');
+            $table->longText('instructions')->nullable()->after('projectable_id');
         });
     }
 
