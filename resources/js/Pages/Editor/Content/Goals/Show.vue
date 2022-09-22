54397ab1cfbc7268c2975b62213836e480b039a9
Jarom Rial <jarom.rial@gmail.com>
2022-09-22 05:59:41 -0600

    fix html alignment

--- resources/js/Pages/Editor/Content/Goals/Show.vue
+++ resources/js/Pages/Editor/Content/Goals/Show.vue
@@ -63,11 +63,11 @@ const destroy = () => {
                             <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2"> {{ goal.show_me_video_url }} </dd>
                         </div>
                         <div class="py-4 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                        <dt class="text-sm font-medium text-gray-500">Project</dt>
-                        <dd class="mt-1 text-sm font-bold text-gray-900 sm:mt-0 sm:col-span-2">
-                            <Link class="hover:text-gray-600" :href="route('editor.projects.show', [goal.project.slug])"> {{ goal.project.name }} </Link>
-                        </dd>
-                    </div>
+                            <dt class="text-sm font-medium text-gray-500">Project</dt>
+                            <dd class="mt-1 text-sm font-bold text-gray-900 sm:mt-0 sm:col-span-2">
+                                <Link class="hover:text-gray-600" :href="route('editor.projects.show', [goal.project.slug])"> {{ goal.project.name }} </Link>
+                            </dd>
+                        </div>
                     </dl>
                 </div>
                 <div class="mt-12">
