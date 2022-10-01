3c69b72bc6dba43e90f7c0999853a8afa5417fba
Jarom Rial <jarom.rial@gmail.com>
2022-09-30 22:28:31 -0600

    update hover state for collections on content index page

--- resources/js/Pages/Editor/Content/Index.vue
+++ resources/js/Pages/Editor/Content/Index.vue
@@ -16,7 +16,7 @@ defineProps({
     <AdminLayout :type="$page.props.auth.user.roles[0]?.name">
         <div class="w-full py-8 px-4 sm:px-6 lg:px-8">
             <ul role="list" class="grid grid-cols-2 gap-6 sm:grid-cols-3 md:grid-cols-5 lg:grid-cols-7">
-                <li v-for="collection in collections" :key="collection.id" class="col-span-1 border border-transparent flex flex-col items-center justify-center rounded-lg bg-white text-center shadow hover:border-indigo-400">
+                <li v-for="collection in collections" :key="collection.id" class="col-span-1 border-2 border-transparent flex flex-col items-center justify-center rounded-lg bg-white text-center shadow hover:border-indigo-400 hover:bg-indigo-50">
                     <Link class="p-6" :href="route('editor.collections.show', [collection.slug])">
                         <div class="flex flex-1 flex-col">
                             <h3 class="text-sm font-medium text-gray-900">{{ collection.name }}</h3>
