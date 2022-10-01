70067df43c1aa341eb7dfed4d6c7287eb5477ac4
Jarom Rial <jarom.rial@gmail.com>
2022-09-30 18:24:27 -0600

    add empty state for when a user has no notifications

--- resources/js/Layouts/Admin.vue
+++ resources/js/Layouts/Admin.vue
@@ -8,6 +8,7 @@ import BreezeResponsiveNavLink from '@/Components/ResponsiveNavLink.vue';
 import { Link } from '@inertiajs/inertia-vue3';
 import VerticalNav from '@/Components/Navigation/Vertical.vue';
 import { BellIcon } from '@heroicons/vue/24/outline';
+import { InboxIcon } from '@heroicons/vue/24/solid';
 import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/vue'
 
 defineProps({
@@ -52,10 +53,15 @@ const showingNavigationDropdown = ref(false);
 
                                 <transition enter-active-class="transition ease-out duration-100" enter-from-class="transform opacity-0 scale-95" enter-to-class="transform opacity-100 scale-100" leave-active-class="transition ease-in duration-75" leave-from-class="transform opacity-100 scale-100" leave-to-class="transform opacity-0 scale-95">
                                     <MenuItems class="absolute right-0 z-10 mt-2 w-72 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                                        <div class="py-1">
+                                        <div v-if="$page.props.notifications.length" class="py-1">
                                             <MenuItem v-for="notification in $page.props.notifications" :key="notification.div" v-slot="{ active }">
                                                 <Link :href="route('notifications.read', [notification])" :class="[active ? 'bg-gray-100 text-gray-900' : 'text-gray-700', notification.read_at === null ? 'font-bold' : 'font-normal', 'block px-4 py-3 text-xs']">{{ notification.data.message }} {{ notification.data.draftable }}</Link>
                                             </MenuItem>
+                                        </div>
+                                        <div v-else class="text-center px-4 py-6">
+                                            <InboxIcon class="mx-auto h-12 w-12 text-indigo-200" />
+                                            <p class="mt-2 text-sm font-medium text-gray-900">No notifications yet</p>
+                                            <p class="mt-1 text-sm text-gray-500">We'll let you know when you have some.</p>
                                         </div>
                                     </MenuItems>
                                 </transition>
