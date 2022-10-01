8c0d0219ffd86b086dedbb2d3313da79d98bb87b
Jarom Rial <jarom.rial@gmail.com>
2022-09-30 21:44:00 -0600

    remove commented code from previous table on editor dashboard

--- resources/js/Pages/Editor/Dashboard.vue
+++ resources/js/Pages/Editor/Dashboard.vue
@@ -38,36 +38,6 @@ const cells = {
                     <TableHead :cells="cells" :actions="false" />
                     <TableBody :cells="cells" :rows="assignments" routeType="assignments" :actions="false" />
                 </Table>
-                <!-- <div class="mt-3 flex flex-col">
-                    <div class="-my-2 -mx-4 overflow-x-auto sm:-mx-6 lg:-mx-8">
-                        <div class="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
-                            <div class="overflow-hidden shadow ring-1 ring-black ring-opacity-5 md:rounded-lg">
-                                <table class="min-w-full divide-y divide-gray-300">
-                                    <thead class="bg-gray-50">
-                                        <tr>
-                                            <th scope="col" class="py-3 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6">Summary</th>
-                                            <th scope="col" class="px-3 py-3 text-left text-sm font-semibold text-gray-900">Status</th>
-                                            <th scope="col" class="px-3 py-3 text-left text-sm font-semibold text-gray-900">Belongs to</th>
-                                            <th scope="col" class="px-3 py-3 text-left text-sm font-semibold text-gray-900">Created</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody class="bg-white">
-                                        <tr v-for="(assignment, assignmentIdx) in assignments" :key="assignment.id" :class="assignmentIdx % 2 === 0 ? undefined : 'bg-gray-50'">
-                                            <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-semibold text-gray-900 hover:text-gray-700 sm:pl-6">
-                                                <Link :href="route('editor.assignments.show', [assignment.id])">{{ assignment.summary }}</Link>
-                                            </td>
-                                            <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
-                                                <Badge :status="assignment.status" />
-                                            </td>
-                                            <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{ assignment.assignable.name }}</td>
-                                            <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{ new Date(assignment.created_at).toLocaleDateString('en-us', { weekday:"long", year:"numeric", month:"short", day:"numeric"}) }}</td>
-                                        </tr>
-                                    </tbody>
-                                </table>
-                            </div>
-                        </div>
-                    </div>
-                </div> -->
             </div>
         </div>
     </AdminLayout>
