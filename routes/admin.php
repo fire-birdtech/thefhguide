1cc5e370247e7940ed20d07eaa6d7c5075e33ad3
Jarom Rial <jarom.rial@gmail.com>
2022-10-02 14:38:45 -0600

    move admin routes to separate file

---
+++ routes/admin.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Admin Web Routes
+|--------------------------------------------------------------------------
+*/
+
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\AssignmentController;
+use App\Http\Controllers\InvitationController;
+use Illuminate\Support\Facades\Route;
+
+Route::group([
+    'middleware' => ['auth', 'verified', 'role:admin|super admin'],
+    'prefix' => 'admin',
+    'as' => 'admin.'
+], function () {
+    Route::get('dashboard', fn () => inertia('Admin/Dashboard'))->name('dashboard');
+    Route::get('editors', [AdminController::class, 'index'])->name('editors.index');
+    Route::get('editors/create', [AdminController::class, 'create'])->name('editors.create');
+    Route::get('editors/show/{user}', [AdminController::class, 'show'])->name('editors.show');
+    Route::get('editors/edit/{user}', [AdminController::class, 'edit'])->name('editors.edit');
+    Route::put('editors/edit/{user}', [AdminController::class, 'update'])->name('editors.update');
+    Route::post('editors/invite', [InvitationController::class, 'store'])->name('editors.invite');
+    Route::resource('invitations', InvitationController::class);
+    Route::post('invitations/resend', [InvitationController::class, 'resend'])->name('invitations.resend');
+    Route::get('assignments', [AssignmentController::class, 'index'])->name('assignments.index');
+    Route::get('assignments/create', [AssignmentController::class, 'create'])->name('assignments.create');
+    Route::post('assignments', [AssignmentController::class, 'store'])->name('assignments.store');
+});
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,5 @@
 <?php
 
-use App\Http\Controllers\AdminController;
-use App\Http\Controllers\AssignmentController;
-use App\Http\Controllers\ContentController;
 use App\Http\Controllers\InvitationController;
 use App\Http\Controllers\NotificationController;
 use Illuminate\Foundation\Application;
@@ -43,25 +40,7 @@ Route::get('/notifications/{id}/read', [NotificationController::class, 'update']
 
 require __DIR__.'/editor.php';
 
-Route::group([
-    'middleware' => ['auth', 'verified', 'role:admin|super admin'],
-    'prefix' => 'admin',
-    'as' => 'admin.'
-], function () {
-    Route::get('dashboard', fn () => inertia('Admin/Dashboard'))->name('dashboard');
-    Route::get('content', [ContentController::class, 'index'])->name('content.index');  
-    Route::get('editors', [AdminController::class, 'index'])->name('editors.index');
-    Route::get('editors/create', [AdminController::class, 'create'])->name('editors.create');
-    Route::get('editors/show/{user}', [AdminController::class, 'show'])->name('editors.show');
-    Route::get('editors/edit/{user}', [AdminController::class, 'edit'])->name('editors.edit');
-    Route::put('editors/edit/{user}', [AdminController::class, 'update'])->name('editors.update');
-    Route::post('editors/invite', [InvitationController::class, 'store'])->name('editors.invite');
-    Route::resource('invitations', InvitationController::class);
-    Route::post('invitations/resend', [InvitationController::class, 'resend'])->name('invitations.resend');
-    Route::get('assignments', [AssignmentController::class, 'index'])->name('assignments.index');
-    Route::get('assignments/create', [AssignmentController::class, 'create'])->name('assignments.create');
-    Route::post('assignments', [AssignmentController::class, 'store'])->name('assignments.store');
-});
+require __DIR__.'/admin.php';
 
 Route::get('expired', fn () => inertia('Errors/Expired'))->name('expired');
 
