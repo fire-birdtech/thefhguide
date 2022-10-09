b3ed2d8ce7f98eb3b465d334946c1589139d08f4
Jarom Rial <jarom.rial@gmail.com>
2022-10-08 20:21:08 -0600

    add empty state on admin dashboard for no assigned editors

--- resources/js/Pages/Admin/Dashboard.vue
+++ resources/js/Pages/Admin/Dashboard.vue
@@ -2,6 +2,7 @@
 import AdminLayout from '@/Layouts/Admin.vue';
 import { Head } from '@inertiajs/inertia-vue3';
 import AdminDashboardEditorCard from '@/Components/Cards/AdminDashboardEditorCard.vue';
+import NoEditors from '@/Components/EmptyStates/NoEditors.vue';
 
 defineProps({
     editors: Array
@@ -14,9 +15,10 @@ defineProps({
     <AdminLayout>
         <div class="w-full py-8 px-4 sm:px-6 lg:px-8">
             <div>
-                <div class="grid grid-cols-1 gap-6 sm:grid-cols-2">
+                <!-- <div class="grid grid-cols-1 gap-6 sm:grid-cols-2">
                     <AdminDashboardEditorCard v-for="editor in editors" :key="editor.id" :editor="editor" />
-                </div>
+                </div> -->
+                <NoEditors />
             </div>
         </div>
     </AdminLayout>
