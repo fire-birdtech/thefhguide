6e39b941e413eab32105a7b5ee4b97f0216487f5
Jarom Rial <jarom.rial@gmail.com>
2022-10-09 07:25:47 -0600

    add time from now string to notification items

--- resources/js/Components/Dropdowns/NotificationDropdown.vue
+++ resources/js/Components/Dropdowns/NotificationDropdown.vue
@@ -3,6 +3,7 @@ import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/vue';
 import { BellIcon } from '@heroicons/vue/24/outline';
 import { ChevronDownIcon, InboxIcon } from '@heroicons/vue/24/solid';
 import { Link } from '@inertiajs/inertia-vue3';
+import NotificationDropdownItem from '@/Components/Dropdowns/NotificationDropdownItem.vue';
 </script>
 
 <template>
@@ -18,11 +19,12 @@ import { Link } from '@inertiajs/inertia-vue3';
         <transition enter-active-class="transition ease-out duration-100" enter-from-class="transform opacity-0 scale-95" enter-to-class="transform opacity-100 scale-100" leave-active-class="transition ease-in duration-75" leave-from-class="transform opacity-100 scale-100" leave-to-class="transform opacity-0 scale-95">
             <MenuItems class="absolute -right-4 z-10 mt-2 w-72 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
                 <div v-if="$page.props.notifications.length" class="py-1">
-                    <MenuItem v-for="notification in $page.props.notifications" :key="notification.id" v-slot="{ active }">
+                    <NotificationDropdownItem v-for="notification in $page.props.notifications" :key="notification.id" :notification="notification" />
+                    <!-- <MenuItem v-for="notification in $page.props.notifications" :key="notification.id" v-slot="{ active }">
                         <Link :href="route('notifications.read', [notification])" :class="[active ? 'bg-gray-100 text-gray-900' : 'text-gray-700', notification.read_at === null ? 'font-bold' : 'font-normal', 'block px-4 py-3 text-xs']">
                             {{ notification.data.message }}
                         </Link>
-                    </MenuItem>
+                    </MenuItem> -->
                 </div>
                 <div v-else class="text-center px-4 py-6">
                     <InboxIcon class="mx-auto h-12 w-12 text-blue-200" />
---
+++ resources/js/Components/Dropdowns/NotificationDropdownItem.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { Link } from '@inertiajs/inertia-vue3';
+import { MenuItem } from '@headlessui/vue';
+import moment from 'moment';
+
+const props = defineProps({
+    notification: Object
+});
+
+const readableDate = () => {
+    return moment(props.notification.updated_at).fromNow();
+}
+</script>
+
+<template>
+    <MenuItem v-slot="{ active }">
+        <Link :href="route('notifications.read', [notification])" :class="[active ? 'bg-gray-100 text-gray-900' : 'text-gray-700', notification.read_at === null ? 'font-bold' : 'font-normal', 'flex flex-col w-full px-4 py-2 text-xs']">
+            {{ notification.data.message }}
+            <span class="mt-1 text-gray-400">
+                {{ readableDate() }}
+            </span>
+        </Link>
+    </MenuItem>
+</template>
