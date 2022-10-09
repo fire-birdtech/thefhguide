08ec79128bb1b46f9abf8a9fac3cc2cff4a201c6
Jarom Rial <jarom.rial@gmail.com>
2022-10-08 21:01:28 -0600

    display editors or empty state if no editors

--- resources/js/Pages/Admin/Dashboard.vue
+++ resources/js/Pages/Admin/Dashboard.vue
@@ -15,10 +15,10 @@ defineProps({
     <AdminLayout>
         <div class="w-full py-8 px-4 sm:px-6 lg:px-8">
             <div>
-                <!-- <div class="grid grid-cols-1 gap-6 sm:grid-cols-2">
+                <div v-if="editors.length" class="grid grid-cols-1 gap-6 sm:grid-cols-2">
                     <AdminDashboardEditorCard v-for="editor in editors" :key="editor.id" :editor="editor" />
-                </div> -->
-                <NoEditors />
+                </div>
+                <NoEditors v-else />
             </div>
         </div>
     </AdminLayout>
