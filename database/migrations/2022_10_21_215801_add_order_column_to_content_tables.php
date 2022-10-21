bccb503de35bef0cbeb588775284d567ba129caf
Jarom Rial <jarom.rial@gmail.com>
2022-10-21 17:48:37 -0600

    add order column to projects and goals

---
+++ database/migrations/2022_10_21_215801_add_order_column_to_content_tables.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('projects', function (Blueprint $table) {
+            $table->unsignedInteger('order')->after('locked');
+        });
+
+        Schema::table('goals', function (Blueprint $table) {
+            $table->unsignedInteger('order')->after('locked');
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('projects', function (Blueprint $table) {
+            $table->dropColumn('order');
+        });
+
+        Schema::table('goals', function (Blueprint $table) {
+            $table->dropColumn('order');
+        });
+    }
+};
