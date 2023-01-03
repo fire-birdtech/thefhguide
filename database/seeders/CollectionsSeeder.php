4bb6d322bc51c28d1bec5f7c7065234c416a423e
Jarom Rial <jarom.rial@gmail.com>
2023-01-02 20:23:25 -0700

    remove unneeded import

--- database/seeders/CollectionsSeeder.php
+++ database/seeders/CollectionsSeeder.php
@@ -8,8 +8,6 @@ use App\Models\Project;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-use function PHPSTORM_META\map;
-
 class CollectionsSeeder extends Seeder
 {
     /**
