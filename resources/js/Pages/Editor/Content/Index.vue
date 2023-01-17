66bcffac28ade612e7436f38661534d0331c3bdc
Jarom Rial <jarom.rial@gmail.com>
2023-01-17 06:25:18 -0700

    add 'Project Collections' header to content index page

--- resources/js/Pages/Editor/Content/Index.vue
+++ resources/js/Pages/Editor/Content/Index.vue
@@ -15,7 +15,8 @@ defineProps({
 
     <AdminLayout>
         <div class="w-full py-8 px-4 sm:px-6 lg:px-8">
-            <ul role="list" class="grid grid-cols-2 gap-6 sm:grid-cols-3 md:grid-cols-5 lg:grid-cols-7">
+            <h3 class="text-lg leading-6 font-medium text-gray-900"> Project Collections </h3>
+            <ul role="list" class="mt-4 grid grid-cols-2 gap-6 sm:grid-cols-3 md:grid-cols-5 lg:grid-cols-7">
                 <li v-for="collection in collections" :key="collection.id" class="col-span-1 border-2 border-transparent flex flex-col items-center justify-center rounded-lg bg-white text-center shadow hover:border-blue-400 hover:bg-blue-50">
                     <Link class="p-6" :href="route('editor.collections.show', [collection.slug])">
                         <div class="flex flex-1 flex-col">
