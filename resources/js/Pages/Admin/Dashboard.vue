50a0eea5e7cc6bc7196dd8d06785929ef2ec051e
Jarom Rial <jarom.rial@gmail.com>
2023-01-17 06:23:17 -0700

    add 'My Editors' header to admin dashboard

--- resources/js/Pages/Admin/Dashboard.vue
+++ resources/js/Pages/Admin/Dashboard.vue
@@ -15,8 +15,11 @@ defineProps({
     <AdminLayout>
         <div class="w-full py-8 px-4 sm:px-6 lg:px-8">
             <div>
-                <div v-if="editors.length" class="grid grid-cols-1 gap-6 sm:grid-cols-2 2xl:grid-cols-3">
-                    <AdminDashboardEditorCard v-for="editor in editors" :key="editor.id" :editor="editor" />
+                <div v-if="editors.length">
+                    <h3 class="text-lg leading-6 font-medium text-gray-900"> My Editors </h3>
+                    <div class="mt-4 grid grid-cols-1 gap-6 sm:grid-cols-2 2xl:grid-cols-3">
+                        <AdminDashboardEditorCard v-for="editor in editors" :key="editor.id" :editor="editor" />
+                    </div>
                 </div>
                 <NoEditors v-else />
             </div>
