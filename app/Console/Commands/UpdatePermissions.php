007f9c2bddb09380cd2c977eef50420004aeb03b
Jarom Rial <jarom.rial@gmail.com>
2023-01-17 07:16:06 -0700

    add artisan command to run the roles and permissions seeder

---
+++ app/Console/Commands/UpdatePermissions.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Artisan;
+
+class UpdatePermissions extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'update:permissions';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Run the roles and permissions seeder';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        Artisan::call("db:seed --class=RolesAndPermissionsSeeder");
+        
+        return Command::SUCCESS;
+    }
+}
