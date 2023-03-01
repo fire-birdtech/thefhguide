dcf1ee0792f14ceadd4b2d38af96f0228bc6c250
Jarom Rial <jarom.rial@gmail.com>
2023-03-01 05:23:09 -0700

    display content on choice show component

--- resources/js/Components/ChoiceShow.vue
+++ resources/js/Components/ChoiceShow.vue
@@ -1,7 +1,6 @@
 <script setup>
 import { Link } from '@inertiajs/vue3';
-import ResourceListWrapper from './Lists/ResourceListWrapper.vue';
-import ResourceListItem from './Lists/ResourceListItemShow.vue';
+import ResourceListItem from '@/Components/Lists/ResourceListItemPublic.vue';
 
 defineProps({
     choice: Object
@@ -11,33 +10,31 @@ defineProps({
 <template>
     <div class="border-t border-gray-200">
         <dl class="sm:divide-y sm:divide-gray-200">
-            <div class="py-4 sm:py-5 sm:grid sm:grid-cols-4 sm:gap-4 sm:px-6">
+            <div class="py-4 sm:py-5 sm:grid sm:grid-cols-8 sm:gap-4 sm:px-6">
                 <dt class="text-sm font-medium text-gray-500">Name</dt>
-                <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-3"> {{ choice.name }} </dd>
+                <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-7"> {{ choice.name }} </dd>
             </div>
-            <div class="py-4 sm:py-5 sm:grid sm:grid-cols-4 sm:gap-4 sm:px-6">
-                <dt class="text-sm font-medium text-gray-500">Summary</dt>
-                <dd class="mt-1 prose max-w-3xl text-sm text-gray-900 sm:mt-0 sm:col-span-3" v-html="choice.summary" />
-            </div>
-            <div class="py-4 sm:py-5 sm:grid sm:grid-cols-4 sm:gap-4 sm:px-6">
-                <dt class="text-sm font-medium text-gray-500">Instructions</dt>
-                <dd class="mt-1 prose max-w-3xl text-sm text-gray-900 sm:mt-0 sm:col-span-3" v-html="choice.instructions" />
-            </div>
-            <div v-if="choice.resources.length" class="py-4 sm:py-5 sm:grid sm:grid-cols-4 sm:gap-4 sm:px-6">
-                <dt class="text-sm font-medium text-gray-500">Resources</dt>
-                <dd class="sm:col-span-3">
-                    <ResourceListWrapper>
-                        <ResourceListItem v-for="resource in choice.resources" :key="resource.id" :resource="resource" />
-                    </ResourceListWrapper>
+            <div class="py-4 sm:py-5 sm:grid sm:grid-cols-8 sm:gap-4 sm:px-6">
+                <dt class="text-sm font-medium text-gray-500">Content</dt>
+                <dd class="mt-1 text-sm text-gray-900 prose max-w-none space-y-6 sm:mt-0 sm:col-span-7">
+                    <template v-for="item in choice.content" :key="item.id">
+                        <div v-if="item.type === 'summary'">
+                            <div class="text-stone-500 font-medium hover:underline">Summary</div>
+                            <div v-html="item.data" />
+                        </div>
+                        <div v-else-if="item.type === 'resources'">
+                            <div class="text-xl font-medium">Resources</div>
+                            <ol>
+                                <ResourceListItem v-for="(resource, index) in item.data" :key="index" :resource="resource" />
+                            </ol>
+                        </div>
+                        <div v-else-if="item.type === 'exercises'">
+                            <div class="text-stone-500 font-medium hover:underline">Exercises</div>
+                            <div v-html="item.data" />
+                        </div>
+                        <div v-else v-html="item.data" />
+                    </template>
                 </dd>
-            </div>
-            <div v-if="choice.review" class="py-4 sm:py-5 sm:grid sm:grid-cols-4 sm:gap-4 sm:px-6">
-                <dt class="text-sm font-medium text-gray-500">Review</dt>
-                <dd class="mt-1 prose max-w-3xl text-sm text-gray-900 sm:mt-0 sm:col-span-3" v-html="choice.review" />
-            </div>
-            <div v-if="choice.exercises" class="py-4 sm:py-5 sm:grid sm:grid-cols-4 sm:gap-4 sm:px-6">
-                <dt class="text-sm font-medium text-gray-500">Exercises</dt>
-                <dd class="mt-1 prose max-w-3xl text-sm text-gray-900 sm:mt-0 sm:col-span-3" v-html="choice.exercises" />
             </div>
         </dl>
     </div>
