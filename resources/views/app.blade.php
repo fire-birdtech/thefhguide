68a0013f1eeb35907c45e4287c95601296d4f282
Jarom Rial <jarom.rial@gmail.com>
2023-08-31 20:08:05 -0600

    add react to base blade file

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,26 +1,25 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title inertia>{{ config('app.name', 'Laravel') }}</title>
+  <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.googleapis.com">
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;700&family=Open+Sans&display=swap" rel="stylesheet">
+  <!-- Fonts -->
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;700&family=Open+Sans&display=swap"
+        rel="stylesheet">
 
-        <!-- Scripts -->
-        @routes
-        @vite('resources/js/app.js')
-        @inertiaHead
-    </head>
-    <body class="font-sans antialiased">
-        @inertia
-
-        <!-- @env ('local')
-            <script src="http://localhost:8080/js/bundle.js"></script>
-        @endenv -->
-    </body>
+  <!-- Scripts -->
+  @routes
+  @vite('resources/js/app.js')
+  @viteReactRefresh
+  @vite(['resources/js/app.tsx', "resources/js/Pages/{$page['component']}.tsx"])
+  @inertiaHead
+</head>
+  <body class="font-sans antialiased">
+    @inertia
+  </body>
 </html>
