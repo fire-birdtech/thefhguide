10781b8a53c8c38c18a2d5c64cc4b1d377afc261
Jarom Rial <jarom.rial@gmail.com>
2023-09-09 12:32:57 -0600

    update primary button to be a link if the href prop is present

--- resources/js/Components/Tables/TableHeader.tsx
+++ resources/js/Components/Tables/TableHeader.tsx
@@ -1,32 +1,30 @@
 import {Header3} from "@/Components/Typography/Headers";
 import PrimaryButton from "@/Components/Buttons/PrimaryButton";
 
-export default function TableHeader({ header, addLink = '', addRoute = '', addText = '', submit }: {
-    header: string,
-    addLink?: string,
-    addRoute?: string,
-    addText?: string,
-    submit?: CallableFunction,
+export default function TableHeader({header, addRoute = '', addText = '', submit}: {
+  header: string,
+  addRoute?: string,
+  addText?: string,
+  submit?: CallableFunction,
 }) {
-    return (
-        <div className="sm:flex sm:items-center">
-            <div className="sm:flex-auto">
-                <Header3>
-                    {header}
-                </Header3>
-            </div>
-            <div className="ml-4 space-x-2">
-                {addLink && addText ? (
-                    <PrimaryButton href={addLink}>
-                        {addText}
-                    </PrimaryButton>
-                ) : null}
-                {addRoute ? (
-                    <PrimaryButton onClick={submit}>
-                        {addText}
-                    </PrimaryButton>
-                ): null}
-            </div>
-        </div>
-    );
+  return (
+    <div className="sm:flex sm:items-center">
+      <div className="sm:flex-auto">
+        <Header3>
+          {header}
+        </Header3>
+      </div>
+      <div className="ml-4 space-x-2">
+        {addRoute !== undefined ? (
+          <PrimaryButton href={addRoute}>
+            {addText}
+          </PrimaryButton>
+        ) : (
+          <PrimaryButton onClick={submit}>
+            {addText}
+          </PrimaryButton>
+        )}
+      </div>
+    </div>
+  );
 }
