79bac4f93f25de00c20da9660c7e6c50ca152fe5
Jarom Rial <jarom.rial@gmail.com>
2023-09-11 06:36:08 -0600

    update content card for content index page

--- resources/js/Components/Cards/Editor/ContentCard.tsx
+++ resources/js/Components/Cards/Editor/ContentCard.tsx
@@ -1,14 +1,18 @@
+import {type ReactElement} from "react";
 import {Link} from "@inertiajs/react";
-import {Header3} from "@/Components/Typography/Headers";
+import {type Collection, type Project} from "@/types";
 
-export default function ContentCard({ content, type }) {
+export default function ContentCard({ content, type }: {
+  content: Collection|Project;
+  type: string;
+}): ReactElement {
   return (
     <li className="col-span-1 border-2 border-transparent flex flex-col items-center justify-center rounded-lg bg-white text-center shadow hover:border-blue-400 hover:bg-blue-50">
-      <Link className="p-6" href={route(type === 'project' ? 'editor.projects.show' : 'editor.collections.show', [content.id])}>
+      <Link className="p-6" href={route(type === 'project' ? 'editor.projects.show' : 'editor.collections.show', content.id)}>
         <div className="flex flex-1 flex-col">
-          <Header3>
+          <p className="text-sm font-medium text-gray-900">
             {content.name}
-          </Header3>
+          </p>
         </div>
       </Link>
     </li>
