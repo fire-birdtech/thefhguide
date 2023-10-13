85e811e337c8a1143f9e9cd82f66e31dbe8f4cc2
Jarom Rial <jarom.rial@proton.me>
2023-10-13 11:02:38 -0600

    update goal preview to react

---
+++ resources/js/Components/Lists/ExpandableChoiceItem.tsx
@@ -0,0 +1,82 @@
+import {type ReactElement, useState} from "react";
+import {type Choice} from "@/types";
+import {MinusIcon, PlusIcon} from "@heroicons/react/24/solid";
+import {Header5} from "@/Components/Typography/Headers";
+import ResourceListItem from "@/Components/Lists/ResourceListItem";
+
+const listStyleAlpha = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+
+export default function ExpandableChoiceItem({ choice }: { choice: Choice }): ReactElement {
+  const [expanded, setExpanded] = useState<boolean>(true);
+  const [showSummary, setShowSummary] = useState<boolean>(false);
+  const [showExercises, setShowExercises] = useState<boolean>(false);
+
+  return (
+    <li>
+      <div onClick={() => { setExpanded(!expanded) }} className="flex items-center justify-between text-stone-400 text-2xl cursor-pointer">
+        <div className="flex items-center">
+          <div className="inline-flex items-center flex-shrink-0 justify-center w-10 h-10 bg-stone-400 text-white font-display font-medium rounded-full">
+            {listStyleAlpha[choice.order - 1]}
+          </div>
+          <h4 className="ml-3 font-display font-medium">
+            {choice.name}
+          </h4>
+        </div>
+        <div className="flex items-center">
+          {expanded ? (
+            <MinusIcon className="h-8 w-8 flex-shrink-0"/>
+          ) : (
+            <PlusIcon className="h-8 w-8 flex-shrink-0"/>
+          )}
+        </div>
+      </div>
+
+      {expanded && (
+        <div className="py-8 prose max-w-none space-y-6">
+          {choice.content.map((item) => {
+            if (item.type === 'summary') {
+              return <>
+                <button
+                  onClick={() => setShowSummary(!showSummary)}
+                  className="text-stone-500 font-medium hover:underline"
+                >
+                  Summary
+                </button>
+                {showSummary && (
+                  <div dangerouslySetInnerHTML={{__html: item.data}}/>
+                )}
+              </>
+            } else if (item.type === 'resources') {
+              return <>
+                <Header5>
+                  Resources
+                </Header5>
+                <ol>
+                  {item.data.map((resource, index) => (
+                    <ResourceListItem resource={resource}/>
+                  ))}
+                </ol>
+              </>
+            } else if (item.type === 'exercises') {
+              return <>
+                <button
+                  onClick={() => setShowExercises(!showExercises)}
+                  className="text-stone-500 font-medium hover:underline"
+                >
+                  Exercises
+                </button>
+                {showExercises && (
+                  <div dangerouslySetInnerHTML={{__html: item.data}}/>
+                )}
+              </>
+            } else if (item.type === 'header') {
+              return <Header5>{item.data}</Header5>
+            } else {
+              return <div dangerouslySetInnerHTML={{ __html: item.data }}/>
+            }
+          })}
+        </div>
+      )}
+    </li>
+  );
+}
---
+++ resources/js/Pages/Editor/Content/Goals/Preview.tsx
@@ -0,0 +1,47 @@
+import {type ReactElement} from "react";
+import {Head} from "@inertiajs/react";
+import ExpandableChoiceItem from "@/Components/Lists/ExpandableChoiceItem";
+
+export default function GoalPreview({ goal }): ReactElement {
+  return <>
+    <Head title={`Preview of ${goal.name}`}/>
+
+    <div className="relative bg-stone-100">
+      <div className="h-56 sm:h-96 md:absolute md:right-0 md:h-full md:w-1/2">
+        <img
+          src={goal.project.cover_image_url}
+          alt=""
+          className="h-full w-full object-cover border border-stone-900"
+        />
+      </div>
+      <div className="relative px-4 py-16 sm:px-6 lg:px-8">
+        <div className="text-center space-y-8 md:w-1/2">
+          <h2 className="text-3xl font-display font-bold tracking-normal text-stone-800 sm:text-4.5xl">
+            Project {goal.project.order}: {goal.project.name}
+          </h2>
+          <h3 className="text-2.5xl font-display font-medium text-stone-800">
+            <strong>
+              Goal {goal.order}:
+            </strong>{' '}
+            {goal.name}
+          </h3>
+        </div>
+      </div>
+    </div>
+
+    <div className="my-8 bg-stone-100 max-w-5xl mx-auto px-4 py-5 sm:px-12">
+      <h4 className="text-2xl font-display font-medium leading-8 text-stone-900 sm:truncate sm:tracking-tight">
+        Goal {goal.order}: {goal.section_name}
+      </h4>
+      <div className="mt-6 prose max-w-5xl" dangerouslySetInnerHTML={{ __html: goal.summary }}/>
+      <h4 className="mt-16 text-2xl font-display font-medium leading-8 text-stone-900 sm:truncate sm:tracking-tight">
+        Choices
+      </h4>
+      <ul className="mt-8 space-y-8">
+        {goal.choices.map((choice) => (
+          <ExpandableChoiceItem choice={choice}/>
+        ))}
+      </ul>
+    </div>
+  </>;
+}
