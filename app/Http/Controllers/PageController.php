dc4e0e6a956efac97b343cad8b4fefbbee18e2fe
Jarom Rial <jarom.rial@proton.me>
2023-11-03 19:11:49 -0600

    display title and main navigation on goal page

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -6,15 +6,21 @@ use App\Models\Goal;
 use App\Models\GoalPage;
 use App\Models\Page;
 use Illuminate\Http\Request;
+use Inertia\Response;
+use Inertia\ResponseFactory;
 
 class PageController extends Controller
 {
-    public function __invoke(Page $page)
+    public function __invoke(Page $page): Response|ResponseFactory|null
     {
         if ($page->type === GoalPage::class) {
+            $goal = Goal::where('page_id', $page->id)->with('project.collection')->first();
             return inertia('Pages/Goal', [
-                'goal' => Goal::where('page_id', $page->id)->with('project')->first(),
+                'goal' => $goal,
+                'title' => "{$goal->name}: {$goal->project->collection->name} Project {$goal->project->order}, Goal {$goal->order}",
             ]);
         }
+
+        return null;
     }
 }
--- resources/js/Pages/Pages/Goal.tsx
+++ resources/js/Pages/Pages/Goal.tsx
@@ -1,7 +1,14 @@
 import {ReactElement} from "react";
+import {Goal, PageProps} from "@/types";
+import {Head} from "@inertiajs/react";
+import MainNavigation from "@/Components/Navigation/Main";
 
-export default function GoalPage(): ReactElement {
+export default function GoalPage({ auth, goal, title }: PageProps<{ goal: Goal, title: string }>): ReactElement {
   return (
-    <div></div>
+    <>
+      <Head title={title}/>
+
+      <MainNavigation/>
+    </>
   );
 }
