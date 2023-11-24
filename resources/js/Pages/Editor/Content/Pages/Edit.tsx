2e8d7145079506c26f47caf4c45981b84e1237e9
Jarom Rial <jarom.rial@proton.me>
2023-11-24 05:48:35 -0700

    add image link grid

--- resources/js/Pages/Editor/Content/Pages/Edit.tsx
+++ resources/js/Pages/Editor/Content/Pages/Edit.tsx
@@ -12,6 +12,7 @@ import PrimaryButton from "@/Components/Buttons/PrimaryButton";
 
 import HeroFullWidth from '../../../../../images/hero-full-width.png'
 import HeroTwoColumn from '../../../../../images/hero-two-column.png'
+import ImageLinkGrid from '../../../../../images/image-link-grid.png'
 
 export default function PagesEdit ({ auth, page }: PageProps<{ page: Page }>): ReactElement {
   const [addContent, setAddContent] = useState<boolean>(true)
@@ -56,7 +57,7 @@ export default function PagesEdit ({ auth, page }: PageProps<{ page: Page }>): R
                 <div className="mt-1 py-2 sm:mt-0 sm:col-span-4">
                   {addContent ? (
                     <div>
-                      <ul className="col-span-3 grid grid-cols-1 gap-x-6 gap-y-6 sm:grid-cols-2 sm:gap-y-10 md:grid-cols-4 xl:gap-x-8">
+                      <ul className="col-span-3 grid grid-cols-1 gap-x-6 gap-y-6 sm:grid-cols-2 sm:gap-y-10 md:grid-cols-3 xl:grid-cols-4s xl:gap-x-8">
                         <li>
                           <div className="group relative before:absolute before:-inset-2.5 before:rounded-[20px] before:bg-gray-50 before:opacity-0 hover:before:opacity-100">
                             <div className="relative aspect-[2/1] overflow-hidden rounded-lg bg-gray-100 ring-1 ring-gray-900/10">
@@ -83,6 +84,22 @@ export default function PagesEdit ({ auth, page }: PageProps<{ page: Page }>): R
                                 <span className="absolute -inset-2.5 z-10"/>
                                 <span className="relative">
                                   Hero Two Column
+                                </span>
+                              </a>
+                            </h4>
+                          </div>
+                        </li>
+
+                        <li>
+                          <div className="group relative before:absolute before:-inset-2.5 before:rounded-[20px] before:bg-gray-50 before:opacity-0 hover:before:opacity-100">
+                            <div className="relative aspect-[2/1] overflow-hidden rounded-lg bg-gray-100 ring-1 ring-gray-900/10">
+                              <img src={ImageLinkGrid} alt="" className="absolute inset-0 h-full w-full"/>
+                            </div>
+                            <h4 className="mt-2 text-sm font-medium text-gray-500 group-hover:text-blue-600">
+                              <a href="#">
+                                <span className="absolute -inset-2.5 z-10"/>
+                                <span className="relative">
+                                  Image Link Grid
                                 </span>
                               </a>
                             </h4>
