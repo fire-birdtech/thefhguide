9e55cb95cc566e250a925fd6969b6a74e73657f3
Jarom Rial <jarom.rial@proton.me>
2023-11-25 13:03:31 -0700

    display full width hero image on pages show page

---
+++ resources/js/Components/Content/Display/FullWidthHero.tsx
@@ -0,0 +1,23 @@
+import {type ReactElement} from "react"
+
+export default function FullWidthHero({ hero }: { hero: { image_url?: string } }): ReactElement {
+  return (
+    <div>
+      <div className="relative overflow-hidden bg-gray-900 px-6 py-20 sm:px-10 sm:py-16 md:px-12 lg:px-20">
+        <img
+          className="absolute inset-0 h-full w-full object-cover scale-125"
+          src={hero.image_url}
+          alt=""
+        />
+        <div className="relative mx-auto max-w-5xl pt-28 space-y-2">
+          <h1 className="text-[#2ec4b6] font-bold text-[2.5rem] leading-[3rem] font-display">
+            Introduction
+          </h1>
+          <button className="text-xl p-4 inline-block rounded transition-all duration-300 ease-in-out bg-[#02253d] text-white border border-transparent hover:bg-white hover:text-[#02253d] hover:border-[#02253d]">
+            Overview Video
+          </button>
+        </div>
+      </div>
+    </div>
+  )
+}
--- resources/js/Pages/Pages/Show.tsx
+++ resources/js/Pages/Pages/Show.tsx
@@ -2,7 +2,8 @@ import {type ReactElement} from "react"
 import {Head} from "@inertiajs/react"
 import MainNavigation from "@/Components/Navigation/Main"
 import {type Page} from "@/types"
-import Container from "@/Components/Container";
+import {ContentSlug} from "@/enums";
+import FullWidthHero from "@/Components/Content/Display/FullWidthHero";
 
 export default function PagesShow({ page, title }: { page: Page, title: string }): ReactElement {
   return (
@@ -11,11 +12,11 @@ export default function PagesShow({ page, title }: { page: Page, title: string }
 
       <MainNavigation/>
 
-      <div className="pt-[8.5rem]">
-        <Container>
-          <h2>{page.name}</h2>
-        </Container>
-      </div>
+    <div className="pt-[8.5rem]">
+      {page.content.map((item, index) => {
+        if (item.type === ContentSlug.HERO_FULL_WIDTH) return <FullWidthHero key={index} hero={item}/>
+      })}
+    </div>
     </>
   )
 }
