a2fef13f51906195341d13d3e2ed7eee2d47cdef
Jarom Rial <jarom.rial@proton.me>
2023-11-25 08:34:28 -0700

    add mediaable type enum

--- resources/js/enums/index.ts
+++ resources/js/enums/index.ts
@@ -12,3 +12,13 @@ export enum CollectionType {
   PROJECT = 'project',
   PAGE = 'page'
 }
+
+export enum ContentSlug {
+  HERO_FULL_WIDTH = 'hero-full-width',
+  HERO_TWO_COLUMN = 'hero-two-column',
+  IMAGE_LINK_GRID = 'image-link-grid',
+}
+
+export enum MediaableType {
+  CHOICE = 'choice'
+}
