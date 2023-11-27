2db97aab56d421210655979677153fcd9e03eb24
Jarom Rial <jarom.rial@proton.me>
2023-11-27 14:22:08 -0700

    adjust padding and prose

--- resources/js/Components/Content/Display/ContentRightImage.tsx
+++ resources/js/Components/Content/Display/ContentRightImage.tsx
@@ -3,7 +3,7 @@ import {type ReactElement} from "react"
 export default function ContentRightImage({ content }: { content: { type?: string, image_url?: string, title?: string, body?: string } }): ReactElement {
   return (
     <section className="py-8">
-      <img src={content.image_url} width="450" alt="" className="mt-16"/>
+      <img src={content.image_url} width="450" alt="" className="mt-14"/>
       <h2>
         {content.title}
       </h2>
--- resources/js/Components/Content/Forms/ArticleWrapper.tsx
+++ resources/js/Components/Content/Forms/ArticleWrapper.tsx
@@ -2,7 +2,7 @@ import {PropsWithChildren, type ReactElement} from "react"
 
 export default function ArticleWrapper({ children }: PropsWithChildren): ReactElement {
   return (
-    <article className="mx-auto max-w-5xl divide-y divide-gray-300 prose prose-headings:font-display prose-headings:font-medium prose-headings:mb-2 prose-h2:mt-0 prose-h2:text-3xl prose-img:float-right prose-img:border prose-img:border-black prose-img:ml-4 prose-img:max-w-md prose-p:last:mb-0">
+    <article className="mx-auto max-w-5xl divide-y divide-gray-300 prose prose-sm prose-headings:font-display prose-headings:font-medium prose-headings:mb-2 prose-h2:mt-0 prose-h2:text-3xl prose-img:float-right prose-img:border prose-img:border-black prose-img:ml-4 prose-img:max-w-md prose-p:last:mb-0">
       {children}
     </article>
   )
