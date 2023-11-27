57f0bcc8c03f755115d999975ee1a1e82e6d5740
Jarom Rial <jarom.rial@proton.me>
2023-11-27 14:11:07 -0700

    display body text for content with right-aligned image

--- resources/js/Components/Content/Display/ContentRightImage.tsx
+++ resources/js/Components/Content/Display/ContentRightImage.tsx
@@ -3,10 +3,11 @@ import {type ReactElement} from "react"
 export default function ContentRightImage({ content }: { content: { type?: string, image_url?: string, title?: string, body?: string } }): ReactElement {
   return (
     <section className="py-8">
-      <img src={content.image_url} width="450" alt=""/>
+      <img src={content.image_url} width="450" alt="" className="mt-16"/>
       <h2>
         {content.title}
       </h2>
+      <div dangerouslySetInnerHTML={{ __html: content.body }}/>
     </section>
   )
 }
