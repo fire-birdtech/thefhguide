6c57e4b050cf95460f433553b56ce44fb7b47056
Jarom Rial <jarom.rial@proton.me>
2023-11-27 13:25:30 -0700

    display image for content with right-aligned image

--- resources/js/Components/Content/Display/ContentRightImage.tsx
+++ resources/js/Components/Content/Display/ContentRightImage.tsx
@@ -3,6 +3,7 @@ import {type ReactElement} from "react"
 export default function ContentRightImage({ content }: { content: { type?: string, image_url?: string, title?: string, body?: string } }): ReactElement {
   return (
     <section className="py-8">
+      <img src={content.image_url} width="450" alt=""/>
       <h2>
         {content.title}
       </h2>
