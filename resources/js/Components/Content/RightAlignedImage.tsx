257d0f60e6ac722e5f497cbf5be056749e25050b
Jarom Rial <jarom.rial@proton.me>
2023-12-07 07:22:30 -0700

    display right-aligned image for a content section

---
+++ resources/js/Components/Content/RightAlignedImage.tsx
@@ -0,0 +1,6 @@
+import {type ReactElement} from "react";
+import {type ContentImage} from "@/types";
+
+export default function RightAlignedImage ({ image }: { image: ContentImage }): ReactElement {
+  return <img src={image.url} width={image.width} alt="" className="mt-14"/>
+}
--- resources/js/Components/ContentSectionShow.tsx
+++ resources/js/Components/ContentSectionShow.tsx
@@ -2,12 +2,14 @@ import {type ReactElement} from "react";
 import {type PageContentElement} from "@/types";
 import {ContentElementType} from "@/enums";
 import SectionTitle from "@/Components/Content/SectionTitle";
+import RightAlignedImage from "@/Components/Content/RightAlignedImage";
 
 export default function ContentSectionShow ({ section }: { section: PageContentElement[] }): ReactElement {
   return (
     <div className="py-8">
       {section.map((element: PageContentElement, index: number) => {
         if (element.type === ContentElementType.TITLE) return <SectionTitle key={index} title={element.data}/>
+        if (element.type === ContentElementType.RIGHT_ALIGNED_IMAGE) return <RightAlignedImage key={index} image={element.data}/>
       })}
     </div>
   )
