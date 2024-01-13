46484c1e354abe532a066aee84920432cf2f2314
Jarom Rial <jarom.rial@proton.me>
2024-01-13 06:44:22 -0700

    display hero and content on editor page show page

--- resources/js/Pages/Editor/Content/Pages/Show.tsx
+++ resources/js/Pages/Editor/Content/Pages/Show.tsx
@@ -5,6 +5,10 @@ import Admin from "@/Layouts/Admin";
 import Container from "@/Components/Container";
 import {Header3} from "@/Components/Typography/Headers";
 import SecondaryButtonWithDropdown from "@/Components/Buttons/SecondaryButtonWithDropdown";
+import PageHero from "@/Components/Content/Display/PageHero";
+import DescriptionListItem from "@/Components/Lists/DescriptionListItem";
+import ArticleWrapper from "@/Components/Content/Display/ArticleWrapper";
+import ContentSectionShow from "@/Components/ContentSectionShow";
 
 type PageShowProps = {
   page: Page
@@ -42,14 +46,19 @@ export default function PageShow ({ auth, page }: PageShowProps): ReactElement {
 
           <div className="mt-4 bg-white shadow overflow-hidden sm:rounded-lg">
             <dl className="sm:divide-y sm:divide-gray-200">
-              <div className="py-4 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                <dt className="text-sm font-medium text-gray-500">
-                  Name
-                </dt>
-                <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-                  {page.name}
-                </dd>
-              </div>
+              <DescriptionListItem term="Name" details={page.name}/>
+              {page.hero !== null && (
+                <DescriptionListItem term="Hero">
+                  <PageHero hero={page.hero}/>
+                </DescriptionListItem>
+              )}
+              <DescriptionListItem term="Content">
+                <ArticleWrapper>
+                  {page.content !== null && page.content.map((item, index) => (
+                    <ContentSectionShow key={index} section={item}/>
+                  ))}
+                </ArticleWrapper>
+              </DescriptionListItem>
             </dl>
           </div>
         </Container>
