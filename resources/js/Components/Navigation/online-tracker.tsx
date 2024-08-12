8f86131a7ac38985bbfe5b75dbbfaf064000b4c9
Jarom Rial <jarom@firebirdtech.dev>
2024-08-12 17:08:02 -0600

    update active state list for group nav

--- resources/js/Components/Navigation/online-tracker.tsx
+++ resources/js/Components/Navigation/online-tracker.tsx
@@ -17,7 +17,7 @@ interface Props {
 const primaryNav: Navigation[] = [
   { label: 'Tracker Home', href: route('dashboard'), components: ['Dashboard'] },
   { label: 'My Account', href: '#', components: [] },
-  { label: 'Groups', href: route('groups'), components: ['Groups/Index'] },
+  { label: 'Groups', href: route('groups'), components: ['Groups/Index', 'Groups/Show'] },
   { label: 'Reports', href: '#', components: [] },
   { label: 'Stars', href: '#', components: [] }
 ]
