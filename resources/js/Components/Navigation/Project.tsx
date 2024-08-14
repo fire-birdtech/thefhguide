9952bdf94e16ab79929d6b812a0463334b320fca
Jarom Rial <jarom@firebirdtech.dev>
2024-08-14 10:03:29 -0600

    update active state logic for project nav

--- resources/js/Components/Navigation/Project.tsx
+++ resources/js/Components/Navigation/Project.tsx
@@ -38,7 +38,7 @@ export default function ProjectNavigation ({ navigation, collectionSlug }: Proje
       <nav className="mx-auto flex items-center flex-wrap gap-x-3 gap-y-2 text-sm px-4 py-3 lg:px-8 lg:py-0" aria-label="Page">
         {navigation.map((item, index) => (
           <Link key={index} href={item.link} className={classNames(
-            window.location.toString() === item.link ? 'font-bold' : '',
+            window.location.toString().includes(item.link) ? 'font-bold' : '',
             'hover:text-[#ff9f1c]'
           )}>
             {item.name}
