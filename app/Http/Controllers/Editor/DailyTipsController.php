876df51bf0927eaf53345cc02afebfad46459028
Jarom Rial <jarom@firebirdtech.dev>
2024-12-01 15:45:53 -0700

    (style) pint

--- app/Http/Controllers/Editor/DailyTipsController.php
+++ app/Http/Controllers/Editor/DailyTipsController.php
@@ -13,16 +13,16 @@ class DailyTipsController extends Controller
 {
     public function index(): Response
     {
-//        $tips = DailyTip::query()
-//            ->get()
-//            ->groupBy(function ($tip) {
-//                return Carbon::parse($tip->created_at)->format('Y');
-//            })
-//            ->map(function ($year) {
-//                return $year->groupBy(function ($tip) {
-//                    return Carbon::parse($tip->created_at)->format('m');
-//                });
-//            });
+        //        $tips = DailyTip::query()
+        //            ->get()
+        //            ->groupBy(function ($tip) {
+        //                return Carbon::parse($tip->created_at)->format('Y');
+        //            })
+        //            ->map(function ($year) {
+        //                return $year->groupBy(function ($tip) {
+        //                    return Carbon::parse($tip->created_at)->format('m');
+        //                });
+        //            });
 
         return inertia('Editor/DailyTips/Index', [
             'tips' => DailyTip::query()->latest()->take(10)->get(),
